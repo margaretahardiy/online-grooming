ef263cdec7740f1b7ef58322a8cc92f95c29d173
Margareta Hardiyanti <mhardiyanti@student.unimelb.edu.au>
2018-05-11 17:19:11 +1000

    Update reminder email

--- resources/views/emailreminder.blade.php
+++ resources/views/emailreminder.blade.php
@@ -2,3 +2,8 @@
 
 <h2> Hi,{{ $name }}!!! </h2>
 <h3>Don't forget your dog's grooming appointment tomorrow with us at {{ $time }}!</h3>
+
+
+<h3> Sincerely,</h3>
+<h3> Tom</h3>
+<h3> Dog Online Groomer</h3>
